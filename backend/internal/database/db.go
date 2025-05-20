4469ebeae96f6ae97eb637191ee709ef5e29d254
Raphael Leveque <raphaellev1102@gmail.com>
2025-05-20 16:04:30 -0300

    Fixing database connection string and adding logging for connection details

--- backend/internal/database/db.go
+++ backend/internal/database/db.go
@@ -4,11 +4,15 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+
+	_ "github.com/lib/pq"
 )
 
 func Connect(host, port, user, password, dbname, sslmode string) (*sql.DB, error) {
 	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbname, sslmode)
-	db, err := sql.Open("postgress", connStr)
+	log.Println(connStr)
+
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
 	}
